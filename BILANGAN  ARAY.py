7b0a8d8b3cec51e7626d29077a1bc55cce6b724a
sandy13novanto <52731767+sandy13novanto@users.noreply.github.com>
2019-07-29 08:39:00 +0700

    Add files via upload

---
+++ BILANGAN  ARAY.py
@@ -0,0 +1,12 @@
+x = [5, 20, 30]
+z = [20, 30, 90]
+
+# Menghitung jumlah nilai semua array
+xTotal = sum(x)
+zTotal = sum(z)
+jumlah = xTotal + zTotal
+print("Jumlah Nilai Kedua Array :", jumlah)
+
+# Menggabungkan data dari kedua array
+print("Gabungan Array")
+print(x + z)
