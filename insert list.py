45fba72162c3d26de7ac1d952e99e42133aeb7b1
sandy13novanto <52731767+sandy13novanto@users.noreply.github.com>
2019-07-29 09:18:33 +0700

    Add files via upload

---
+++ insert list.py
@@ -0,0 +1,12 @@
+list = [1,4,2,5,3,8,6,9,7,11,10]
+
+print("Mengecek List Awal")
+print(list)
+
+print("Menambahkan Data List dengan Insert")
+list.insert(2,70)
+print(list)
+
+print("Mengurutkan List")
+list.sort()
+print(list)
